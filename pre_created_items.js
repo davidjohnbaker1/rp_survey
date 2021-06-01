58c49b6d3d55b9d117520b740307c331aa824588
davidjohnbaker <davidjohnbaker1@gmail.com>
2021-06-01 16:36:16 -0500

    added 1 june basics

---
+++ pre_created_items.js
@@ -0,0 +1,259 @@
+
+var ess_a = {
+  type: 'survey-likert',
+    preamble: "Finding/singing tonic when hearing some music",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+
+// 
+
+var ess_b = {
+  type: 'survey-likert',
+    preamble: "Singing/identifying an interval from a given note (without necessarily labeling individual notes)",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+var ess_c = {
+  type: 'survey-likert',
+    preamble: "Knowing whether two heard intervals are the same size",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+var ess_d = {
+  type: 'survey-likert',
+    preamble: "Identifying harmonic function without identifying notes",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+var ess_e = {
+  type: 'survey-likert',
+    preamble: "Hearing a melody and playing it by ear on an instrument",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+var ess_f = {
+  type: 'survey-likert',
+    preamble: "Singing on functional syllables (scale degrees or moveable-Do)",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+var ess_g = {
+  type: 'survey-likert',
+    preamble: "Hearing a melody and transposing it by ear",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+
+var ess_h = {
+  type: 'survey-likert',
+    preamble: "Hearing a melody and being able to identify particular scale degrees",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+var ess_i = {
+  type: 'survey-likert',
+    preamble: "Hearing some note and being able to remember its sound at a later time",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+
+var ess_j = {
+  type: 'survey-likert',
+    preamble: "Always knowing what one pitch-class (or pitch) sounds like and identifying others from it",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+
+var ess_k = {
+  type: 'survey-likert',
+    preamble: "Being able to sing the starting note of a favorite song",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+
+var ess_l = {
+  type: 'survey-likert',
+    preamble: "Being able to sing a melody in a key other than notated.",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+
+var ess_m = {
+  type: 'survey-likert',
+    preamble: "Identifying intervals and chord qualities.",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+
+
+var ess_n = {
+  type: 'survey-likert',
+    preamble: "Identifying tonic and scale degrees in a melody or harmonic progression",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+
+var ess_o = {
+  type: 'survey-likert',
+    preamble: "Error detection when comparing a melody or progression and its transposition.",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+var ess_p = {
+  type: 'survey-likert',
+    preamble: "Singing at sight in a key other than notated.",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+var ess_q = {
+  type: 'survey-likert',
+    preamble: "Taking melodic/harmonic dictation in a key other than that in which it is heard.",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+var ess_r = {
+  type: 'survey-likert',
+    preamble: "Identifying relationships between keys (e.g., modulates to the dominant)",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
+
+
+var ess_s = {
+  type: 'survey-likert',
+    preamble: "Add stuff about remembering a note 1 minute later, etc.",
+  questions: [
+      {prompt: "1", name: 'often', labels: practice_scale_7},
+      {prompt: "2", name: 'rp', labels: rp_scale_7},
+      {prompt: "3", name: 'ap', labels: ap_scale_7},
+      {prompt: "4", name: 'learn', labels: teach_scale_7},
+      {prompt: "5", name: 'teach', labels: learn_scale_7}
+  ],
+  randomize_question_order: false 
+};
---
+++ scales.js
@@ -0,0 +1,17 @@
+// Scales
+
+var yes_no = ["Yes", "No"];
+
+var scale_7 = ["Strongly Disagree", "","", "Neutral", "", "", "Strongly Agree"];
+
+var scale_priority_5 = ["A Very Low Priority", "", "Neither a High or Low Priority", "",  "A Very High Priority"];
+    
+var practice_scale_7 = ["I never practice this with students", "", "", "I sometimes practice this with students", "", "", "I very frequently practice this with students."];
+
+var rp_scale_7 = ["This is not an RP skill", "", "", "This skill partially fosters RP", "", "", "This skill is the essence of RP"];
+
+var ap_scale_7 = ["This is not an AP skill", "", "", "This skill partially fosters AP", "", "", "This skill is the essence of AP"];
+
+var teach_scale_7 = ["This is an easy skill for students to learn", "", "", "Middle", "", "", "This is a difficult skill for students to learn"];
+
+var learn_scale_7 = ["This is an easy skill to teach students", "", "", "Mild Sauce", "", "", "This is difficult to teach"];
