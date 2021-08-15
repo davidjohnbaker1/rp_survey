7f47b0d4544968935dbb257b4682e71911d5f6b6
davidjohnbaker1 <davidjohnbaker1@gmail.com>
2021-08-15 14:40:49 +0100

    pre irb

--- scales.js
+++ scales.js
@@ -1,21 +1,21 @@
 // Scales
 
-var yes_no = ["Yes", "No"];
+var yes_no = ["Yes", "No", "NR"];
 
-var yes_no_na = ["Yes", "No", "Not Sure", "Not Applicable"];
+var yes_no_na = ["Yes", "No", "Not Sure", "Not Applicable", "NR"];
 
-var scale_7 = ["Strongly Disagree", "","", "Neutral", "", "", "Strongly Agree"];
+var scale_7 = ["Strongly Disagree", "","", "Neutral", "", "", "Strongly Agree","NR"];
 
-var scale_7_na = ["Strongly Disagree", "","", "Neutral", "", "", "Strongly Agree", "Not Applicable"];
+var scale_7_na = ["Strongly Disagree", "","", "Neutral", "", "", "Strongly Agree", "Not Applicable","NR"];
 
-var scale_priority_5 = ["A Very Low Priority", "", "Neither a High or Low Priority", "",  "A Very High Priority"];
+var scale_priority_5 = ["A Very Low Priority", "", "Neither a High or Low Priority", "",  "A Very High Priority", "NR"];
     
-var practice_scale_7 = ["I never practice this with students", "", "", "I sometimes practice this with students", "", "", "I very frequently practice this with students."];
+var practice_scale_7 = ["I never practice this with students", "", "", "I sometimes practice this with students", "", "", "I very frequently practice this with students.", "NR"];
 
-var rp_scale_7 = ["This is not an RP skill", "", "", "This skill partially fosters RP", "", "", "This skill is the essence of RP"];
+var rp_scale_7 = ["This is not an RP skill", "", "", "This skill partially fosters RP", "", "", "This skill is the essence of RP", "NR"];
 
-var ap_scale_7 = ["This is not an AP skill", "", "", "This skill partially fosters AP", "", "", "This skill is the essence of AP"];
+var ap_scale_7 = ["This is not an AP skill", "", "", "This skill partially fosters AP", "", "", "This skill is the essence of AP", "NR"];
 
-var teach_scale_7 = ["This is an easy skill for students to learn", "", "", "Middle", "", "", "This is a difficult skill for students to learn"];
+var teach_scale_7 = ["This is an easy skill for students to learn", "", "", "Middle", "", "", "This is a difficult skill for students to learn", "NR"];
 
-var learn_scale_7 = ["This is an easy skill to teach students", "", "", "Mild Sauce", "", "", "This is difficult to teach"];
+var learn_scale_7 = ["This is an easy skill to teach students", "", "", "Mild Sauce", "", "", "This is difficult to teach", "NR"];
